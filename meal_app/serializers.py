fa70a28ccf77c0f1fe7bd6e380c5571abccdd0cd
BrianOdhiambo <brianodhiambo530@gmail.com>
2023-11-09 12:35:22 +0300

    Added the meal application

---
+++ meal_app/serializers.py
@@ -0,0 +1,66 @@
+from rest_framework import serializers
+from authentication.serializers import UserSerializer
+
+from .models import Meal, Menu, Customer, Admin, Order
+
+class AdminSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Admin
+        fields = ['first_name', 'last_name']
+
+class CustomerSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Customer
+        fields = ['first_name', 'last_name']
+
+class AddMealSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Meal
+        fields = ['id', 'name', 'description', 'price', 'image']
+
+class MealSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Meal
+        fields = ['id', 'name', 'description', 'price', 'image', 'created_at']
+
+class CreateMenuSerializer(serializers.ModelSerializer):
+
+    menu_meals = MealSerializer(many=True)
+
+    class Meta:
+        model = Menu
+        fields = ['id', 'menu_meals', 'date']
+
+    def create(self, validated_data):
+        admin = Admin.objects.get(user_id=self.context['admin_id'])
+        menu = Menu.objects.create(created_by=admin, **validated_data)
+        return menu
+
+class MenuSerializer(serializers.ModelSerializer):
+
+    menu_meals = MealSerializer(many=True)
+    created_by = AdminSerializer()
+
+    class Meta:
+        model = Menu
+        fields = ['id', 'menu_meals', 'date', 'created_by']
+
+class AddOrderSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Order
+        fields = ['id', 'customer', 'menu', 'chosen_meal']
+
+class OrderSerializer(serializers.ModelSerializer):
+
+    customer = CustomerSerializer()
+    menu = MenuSerializer()
+    chosen_meal = MealSerializer()
+
+    class Meta:
+        model = Order
+        fields = ['id', 'customer', 'menu', 'chosen_meal', 'created_at']
